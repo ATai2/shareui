6bd85d6d17d8bf720d0f7bbc48c501e7c01d7d25
ATai2 <1114677814@qq.com>
2019-01-31 00:52:58 +0800

    after log

--- src/main/java/com/ppx/shareui/config/DataSourceConfig.java
+++ src/main/java/com/ppx/shareui/config/DataSourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -15,8 +16,8 @@ import javax.sql.DataSource;
  */
 @Component
 @Configuration
-//@EnableJpaRepositories({ "org.activiti.app.repository" })
-//@EnableTransactionManagement
+@EnableJpaRepositories({ "org.activiti.app.repository" })
+@EnableTransactionManagement
 public class DataSourceConfig {
     @Autowired
     Environment env;
@@ -31,4 +32,10 @@ public class DataSourceConfig {
 
         return dataSource;
     }
+
+
+
+
+
+
 }
