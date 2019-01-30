69f647c440eff5f7d0b63efa663a3573c5a54e62
ATai2 <1114677814@qq.com>
2019-01-31 01:19:33 +0800

    remoce security

--- src/main/java/com/ppx/shareui/ShareuiApplication.java
+++ src/main/java/com/ppx/shareui/ShareuiApplication.java
@@ -1,20 +1,30 @@
 package com.ppx.shareui;
 
-import org.activiti.spring.boot.JpaProcessEngineAutoConfiguration;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityFilterAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 
 /**
  * @author Administrator
  */
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,
+@SpringBootApplication(exclude = {
+        SecurityAutoConfiguration.class,
         LiquibaseAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class,
 //        JpaProcessEngineAutoConfiguration.class,
+//        SecurityAutoConfiguration.class,
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        UserDetailsServiceAutoConfiguration.class,
+        SecurityFilterAutoConfiguration.class,
+        ReactiveSecurityAutoConfiguration.class,
+        ReactiveUserDetailsServiceAutoConfiguration.class,
 })
 public class ShareuiApplication {
 
