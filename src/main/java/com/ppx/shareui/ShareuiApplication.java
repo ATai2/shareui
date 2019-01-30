d99a61b943e8c0490b45bd48a58d5328220935e5
ATai2 <1114677814@qq.com>
2019-01-31 00:24:34 +0800

    1

--- src/main/java/com/ppx/shareui/ShareuiApplication.java
+++ src/main/java/com/ppx/shareui/ShareuiApplication.java
@@ -1,9 +1,14 @@
 package com.ppx.shareui;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class,})
 public class ShareuiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ppx/shareui/controller/TestController.java
+++ src/main/java/com/ppx/shareui/controller/TestController.java
@@ -1,8 +1,15 @@
 package com.ppx.shareui.controller;
 
+import com.ppx.shareui.config.DataSourceConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.impl.persistence.entity.UserEntityImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author Administrator
@@ -11,9 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/*")
 public class TestController {
 
+    @Autowired
+    private DataSourceConfig dataSourceConfig;
+    @Autowired
+    private IdentityService identityService;
     @RequestMapping("/user")
     @ResponseBody
-    public String getUser(){
+    public String getUser(HttpServletRequest request){
+        System.out.println(dataSourceConfig);
+        System.out.println(dataSourceConfig.getDataSource());
+        UserEntityImpl userEntity=new UserEntityImpl();
+        userEntity.setId(request.getParameter("id"));
+        userEntity.setRevision(0);
+        identityService.saveUser(userEntity);
         return "{\"login\":\"shareui\"}";
     }
 }
